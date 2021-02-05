1b7f3960079f9f7730e78781ff1c667ca908ce26
Oleh T <olezhek_85@list.ru>
2021-02-06 01:41:00 +0200

    Routes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from "react";
 import {
   AppBar,
   Toolbar,
@@ -7,13 +8,20 @@ import {
 } from "@material-ui/core";
 import { Menu as MenuIcon } from "@material-ui/icons";
 import { makeStyles } from "@material-ui/core/styles";
+import { Link as RouterLink } from "react-router-dom";
 
-import "./App.css";
-import logo from "./logo.svg";
+import {
+  SIGN_UP_PATH,
+  SIGN_IN_PATH,
+  SIGN_IN_LABEL,
+  SIGN_UP_LABEL,
+} from "../routes";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    backgroundColor: "#282c34",
+    minHeight: "100vh",
   },
   menuButton: {
     marginRight: theme.spacing(2),
@@ -26,7 +34,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const App = () => {
+const Navbar = ({ children }: { children: ReactNode }) => {
   const classes = useStyles();
 
   return (
@@ -44,28 +52,17 @@ const App = () => {
           <Typography variant="h6" className={classes.title}>
             Hillel
           </Typography>
-          <Button color="inherit">Sign in</Button>
-          <Button color="inherit">Sign up</Button>
+          <Button color="inherit" component={RouterLink} to={SIGN_IN_PATH}>
+            {SIGN_IN_LABEL}
+          </Button>
+          <Button color="inherit" component={RouterLink} to={SIGN_UP_PATH}>
+            {SIGN_UP_LABEL}
+          </Button>
         </Toolbar>
       </AppBar>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      {children}
     </div>
   );
 };
 
-export default App;
+export { Navbar };
